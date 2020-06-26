15f13acbb4288099675b48db290923a20fed7971
Obakeng <obeezus@gmail.com>
2020-06-26 16:07:10 +0200

    status bar

--- front_end/src/components/regions/Home/bar.js
+++ front_end/src/components/regions/Home/bar.js
@@ -2,10 +2,11 @@ import React from "react";
 import styled from "styled-components";
 
 export default function Bar() {
-  return <BarContainer></BarContainer>;
+  return <BarContainer>Popular Titles</BarContainer>;
 }
 
 const BarContainer = styled.footer`
   background: grey;
   height: 4rem;
+  color: white;
 `;
